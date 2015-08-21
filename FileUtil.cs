283f0452a81ecaadf635de727f5ef0474080432f
fly1986 <413062709@qq.com>
2015-08-21 14:18:58 +0800

    FileUtil.cs

---
+++ FileUtil.cs
@@ -0,0 +1,319 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CommonCls
+{
+    /// <summary>
+    /// 文件相关 操作类（获取绝对路径、创建目录、验证文件是否存在、获取文件后缀名）等
+    /// </summary>
+    public static class FileUtil
+    {
+
+
+        /// <summary>
+        /// 获得当前绝对路径
+        /// </summary>
+        /// <param name="strPath">指定的路径</param>
+        /// <returns>绝对路径</returns>
+        public static string GetMapPath(string strPath)
+        {
+            if (HttpContext.Current != null)
+            {
+                return HttpContext.Current.Server.MapPath(strPath);
+            }
+            else //非web程序引用
+            {
+                strPath = strPath.Replace("/", "\\");
+                if (strPath.StartsWith("\\"))
+                {
+                    strPath = strPath.Substring(strPath.IndexOf('\\', 1)).TrimStart('\\');
+                }
+                return System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, strPath);
+            }
+        }
+
+        /// <summary>
+        /// 返回文件是否存在
+        /// </summary>
+        /// <param name="filename">文件名</param>
+        /// <returns>是否存在</returns>
+        public static bool FileExists(string filename)
+        {
+            return System.IO.File.Exists(filename);
+        }
+
+
+        /// <summary>
+        /// 以指定的ContentType输出指定文件文件
+        /// </summary>
+        /// <param name="filepath">文件路径</param>
+        /// <param name="filename">输出的文件名</param>
+        /// <param name="filetype">将文件输出时设置的ContentType</param>
+        public static void ResponseFile(string filepath, string filename, string filetype)
+        {
+            Stream iStream = null;
+
+            // 缓冲区为10k
+            byte[] buffer = new Byte[10000];
+            // 文件长度
+            int length;
+            // 需要读的数据长度
+            long dataToRead;
+
+            try
+            {
+                // 打开文件
+                iStream = new FileStream(filepath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+
+                // 需要读的数据长度
+                dataToRead = iStream.Length;
+
+                HttpContext.Current.Response.ContentType = filetype;
+                if (HttpContext.Current.Request.ServerVariables["HTTP_USER_AGENT"].IndexOf("MSIE") > -1)
+                    HttpContext.Current.Response.AddHeader("Content-Disposition", "attachment;filename=" + ClsRequest.UrlEncode(filename.Trim()).Replace("+", " "));
+                else
+                    HttpContext.Current.Response.AddHeader("Content-Disposition", "attachment;filename=" + filename.Trim());
+
+                while (dataToRead > 0)
+                {
+                    // 检查客户端是否还处于连接状态
+                    if (HttpContext.Current.Response.IsClientConnected)
+                    {
+                        length = iStream.Read(buffer, 0, 10000);
+                        HttpContext.Current.Response.OutputStream.Write(buffer, 0, length);
+                        HttpContext.Current.Response.Flush();
+                        buffer = new Byte[10000];
+                        dataToRead = dataToRead - length;
+                    }
+                    else
+                    {
+                        // 如果不再连接则跳出死循环
+                        dataToRead = -1;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                HttpContext.Current.Response.Write("Error : " + ex.Message);
+            }
+            finally
+            {
+                if (iStream != null)
+                {
+                    // 关闭文件
+                    iStream.Close();
+                }
+            }
+            HttpContext.Current.Response.End();
+        }
+
+        /// <summary>
+        /// 判断文件名是否为浏览器可以直接显示的图片文件名
+        /// </summary>
+        /// <param name="filename">文件名</param>
+        /// <returns>是否可以直接显示</returns>
+        public static bool IsImgFilename(string filename)
+        {
+            filename = filename.Trim();
+            if (filename.EndsWith(".") || filename.IndexOf(".") == -1)
+                return false;
+
+            string extname = filename.Substring(filename.LastIndexOf(".") + 1).ToLower();
+            return (extname == "jpg" || extname == "jpeg" || extname == "png" || extname == "bmp" || extname == "gif");
+        }
+
+        /// <summary>
+        /// 判断文件流是否为UTF8字符集
+        /// </summary>
+        /// <param name="sbInputStream">文件流</param>
+        /// <returns>判断结果</returns>
+        private static bool IsUTF8(FileStream sbInputStream)
+        {
+            int i;
+            byte cOctets;  // octets to go in this UTF-8 encoded character 
+            byte chr;
+            bool bAllAscii = true;
+            long iLen = sbInputStream.Length;
+
+            cOctets = 0;
+            for (i = 0; i < iLen; i++)
+            {
+                chr = (byte)sbInputStream.ReadByte();
+
+                if ((chr & 0x80) != 0) bAllAscii = false;
+
+                if (cOctets == 0)
+                {
+                    if (chr >= 0x80)
+                    {
+                        do
+                        {
+                            chr <<= 1;
+                            cOctets++;
+                        }
+                        while ((chr & 0x80) != 0);
+
+                        cOctets--;
+                        if (cOctets == 0)
+                            return false;
+                    }
+                }
+                else
+                {
+                    if ((chr & 0xC0) != 0x80)
+                        return false;
+
+                    cOctets--;
+                }
+            }
+
+            if (cOctets > 0)
+                return false;
+
+            if (bAllAscii)
+                return false;
+
+            return true;
+        }
+
+
+        /// <summary>
+        /// 备份文件
+        /// </summary>
+        /// <param name="sourceFileName">源文件名</param>
+        /// <param name="destFileName">目标文件名</param>
+        /// <param name="overwrite">当目标文件存在时是否覆盖</param>
+        /// <returns>操作是否成功</returns
+        public static bool BackupFile(string sourceFileName, string destFileName, bool overwrite)
+        {
+            if (!System.IO.File.Exists(sourceFileName))
+                throw new FileNotFoundException(sourceFileName + "文件不存在！");
+
+            if (!overwrite && System.IO.File.Exists(destFileName))
+                return false;
+
+            try
+            {
+                System.IO.File.Copy(sourceFileName, destFileName, true);
+                return true;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+
+        /// <summary>
+        /// 备份文件,当目标文件存在时覆盖
+        /// </summary>
+        /// <param name="sourceFileName">源文件名</param>
+        /// <param name="destFileName">目标文件名</param>
+        /// <returns>操作是否成功</returns>
+        public static bool BackupFile(string sourceFileName, string destFileName)
+        {
+            return BackupFile(sourceFileName, destFileName, true);
+        }
+
+
+        /// <summary>
+        /// 恢复文件
+        /// </summary>
+        /// <param name="backupFileName">备份文件名</param>
+        /// <param name="targetFileName">要恢复的文件名</param>
+        /// <param name="backupTargetFileName">要恢复文件再次备份的名称,如果为null,则不再备份恢复文件</param>
+        /// <returns>操作是否成功</returns>
+        public static bool RestoreFile(string backupFileName, string targetFileName, string backupTargetFileName)
+        {
+            try
+            {
+                if (!System.IO.File.Exists(backupFileName))
+                    throw new FileNotFoundException(backupFileName + "文件不存在！");
+
+                if (backupTargetFileName != null)
+                {
+                    if (!System.IO.File.Exists(targetFileName))
+                        throw new FileNotFoundException(targetFileName + "文件不存在！无法备份此文件！");
+                    else
+                        System.IO.File.Copy(targetFileName, backupTargetFileName, true);
+                }
+                System.IO.File.Delete(targetFileName);
+                System.IO.File.Copy(backupFileName, targetFileName);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return true;
+        }
+
+        public static bool RestoreFile(string backupFileName, string targetFileName)
+        {
+            return RestoreFile(backupFileName, targetFileName, null);
+        }
+
+
+        /// <summary>
+        /// 获取指定文件的扩展名
+        /// </summary>
+        /// <param name="fileName">指定文件名</param>
+        /// <returns>扩展名</returns>
+        public static string GetFileExtName(string fileName)
+        {
+            if (Validator.StrIsNullOrEmpty(fileName) || fileName.IndexOf('.') <= 0)
+                return "";
+
+            fileName = fileName.ToLower().Trim();
+
+            return fileName.Substring(fileName.LastIndexOf('.'), fileName.Length - fileName.LastIndexOf('.'));
+        }
+
+
+
+        /// <summary>
+        /// Writes ETag and Last-Modified headers and sets the conditional get headers.
+        /// </summary>
+        /// <param name="date">
+        /// The date for the headers.
+        /// </param>
+        /// <returns>
+        /// The set conditional get headers.
+        /// </returns>
+        public static bool SetConditionalGetHeaders(DateTime date)
+        {
+            // SetLastModified() below will throw an error if the 'date' is a future date.
+            // If the date is 1/1/0001, Mono will throw a 404 error
+            if (date > DateTime.Now || date.Year < 1900)
+            {
+                date = DateTime.Now;
+            }
+
+            var response = HttpContext.Current.Response;
+            var request = HttpContext.Current.Request;
+
+            var etag = string.Format("\"{0}\"", date.Ticks);
+            var incomingEtag = request.Headers["If-None-Match"];
+
+            DateTime incomingLastModifiedDate;
+            DateTime.TryParse(request.Headers["If-Modified-Since"], out incomingLastModifiedDate);
+
+            response.Cache.SetLastModified(date);
+            response.Cache.SetCacheability(HttpCacheability.Public);
+            response.Cache.SetETag(etag);
+
+            if (String.Compare(incomingEtag, etag) == 0 || incomingLastModifiedDate == date)
+            {
+                response.Clear();
+                response.StatusCode = (int)System.Net.HttpStatusCode.NotModified;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
